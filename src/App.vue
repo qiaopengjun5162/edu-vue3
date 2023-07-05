d1fd6a0cca7a54ef0c0d75fb63388c90b798c348
QiaoPengjun5162 <qiaopengjun0@gmail.com>
2023-07-05 23:52:12 +0800

    接口请求与测试

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,21 @@
 <script setup lang="ts">
 import { RouterView } from "vue-router";
+import request from "./utils/request";
+
+request({
+  method: "GET",
+  // http://localhost:8080/front/ad/getAdList
+  url: "/front/ad/getAdList",
+}).then((res) => {
+  console.log(res)
+}).catch((err) => {
+  console.log(err)
+})
 </script>
 
 <template>
   <RouterView />
 </template>
 
-<style scoped></style>
+<style scoped>
+</style>
