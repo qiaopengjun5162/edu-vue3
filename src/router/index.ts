2b80adb6fbf89e95d4aae215b52a6d1ca87abe3b
QiaoPengjun5162 <qiaopengjun0@gmail.com>
2023-07-09 10:54:39 +0800

    登录页面

--- src/router/index.ts
+++ src/router/index.ts
@@ -6,6 +6,11 @@ const router: Router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
     routes: [
         {
+            path: "/login",
+            name: "login",
+            component: () => import("../views/login/LoginView.vue")
+        },
+        {
             path: "/",
             name: "home",
             component: AppLayout,
---
+++ src/views/login/LoginView.vue
@@ -0,0 +1,56 @@
+<script setup lang="ts">
+import { ElMessage } from 'element-plus';
+import { reactive } from 'vue';
+// 表单响应式数据
+const form = reactive({ // 可以使用 ref
+    phone: "18201288771",
+    password: "123",
+})
+// 登录事件处理
+const onSubmit = () => {
+    ElMessage.success("提交表单数据")
+}
+</script>
+
+<template>
+    <div class="login">
+        <el-form :model="form" label-width="120px" label-position="top" size="large">
+            <h2>登录</h2>
+            <el-form-item label="手机号">
+                <el-input v-model="form.phone" />
+            </el-form-item>
+            <el-form-item label="密码">
+                <el-input v-model="form.password" />
+            </el-form-item>
+            <el-form-item>
+                <el-button type="primary" @click="onSubmit">登录</el-button>
+            </el-form-item>
+        </el-form>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.login {
+    background-color: beige;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    .el-form {
+        width: 300px;
+        background-color: #fff;
+        padding: 30px;
+        border-radius: 10px;
+
+        .el-form-item {
+            margin-top: 20px;
+        }
+
+        .el-button {
+            width: 100%;
+            margin-top: 30px;
+        }
+    }
+}
+</style>
